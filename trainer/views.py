22b5044be873c8154f6cec07e6561cba32118920
Kalpesh Tawde <kalpeshtawde@outlook.com>
2018-09-06 22:55:52 +0530

    initial urls setup

--- trainer/views.py
+++ trainer/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse("<H1>Corporate Trainer</H1>")
+
+def name(request):
+    return HttpResponse("<H1>Corporate Trainer: Kalpesh Tawde</H1>")
