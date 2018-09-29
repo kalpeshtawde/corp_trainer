c83c6ed6cc971106d1a3753ec656d8c586f273e1
Kalpesh Tawde <kalpeshtawde@outlook.com>
2018-09-29 10:12:34 +0530

    star rating app deleted

--- corp_trainer/settings.py
+++ corp_trainer/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'trainer.apps.TrainerConfig',
-    'star_ratings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
