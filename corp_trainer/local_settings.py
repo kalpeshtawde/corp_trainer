a162b535b33260ea330432aecd92a9c2d65206d6
kalpeshtawde <kalpeshtawde@outlook.com>
2018-11-15 07:09:12 +0000

    Update local_settings.py

--- corp_trainer/local_settings.py
+++ corp_trainer/local_settings.py
@@ -3,12 +3,7 @@
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tosdb',
-        'USER': 'tos',
-        'PASSWORD': 'ratnagiri',
-        'HOST': 'localhost',
-        'PORT': '5432',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
