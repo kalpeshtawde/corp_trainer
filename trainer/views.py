c24fd32c55336ee30c0398f1ae57a293abef5ede
Kalpesh Tawde <kalpeshtawde@outlook.com>
2019-02-01 23:14:07 +0530

    Detail page modified to get skills list

--- trainer/views.py
+++ trainer/views.py
@@ -63,6 +63,7 @@ class DetailView(generic.DetailView):
         detail_user = Profile.objects.filter(pk=self.kwargs['pk'])[0].user
         context['detail_timeline'] = Timeline.objects.filter(user=detail_user)
         context['detail_experience'] = Experience.objects.filter(user=detail_user)
+        context['detail_skill'] = Skill.objects.filter(user=detail_user)
         context['message_form'] = MessageForm
         return context
 
