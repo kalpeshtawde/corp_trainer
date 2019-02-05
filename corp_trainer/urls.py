b30bbc023bcf51173df010230613664d755bcf1e
Kalpesh Dnyandeo Tawde <ktawde@tripwire.com>
2019-02-05 16:44:11 +0530

    Handling of website url redirect to home page

--- corp_trainer/urls.py
+++ corp_trainer/urls.py
@@ -2,10 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^trainer/', include('trainer.urls')),
+    # Handles raw website url
+    url(r'^$', RedirectView.as_view(url='trainer/')),
 ]
 
 if settings.DEBUG:
